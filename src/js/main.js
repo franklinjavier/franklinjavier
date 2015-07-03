2e6df5dc9ffce628b96ac432e1ac36db15c0b282
Franklin Javier <franklinjalves@gmail.com>
2015-07-03 13:53:54 -0300

    Improve UX with animation

--- src/js/main.js
+++ src/js/main.js
@@ -15,17 +15,32 @@
     if ( a.hostname !== window.location.hostname ) {
 
         a.target = '_blank';
-        a.onclick = function() {
+        a.onclick = function( e ) {
           track('external', this.href);
+          animate( e );
         };
     } else {
 
-      a.onclick = function() {
+      a.onclick = function( e ) {
         track('post', this.href);
+        animate( e );
       };
     }
 
   });
+
+
+  function animate( e ) {
+    e.preventDefault();
+    var el = e.target || e.srcElement;
+
+    document.body.className += ' animated fadeOut';
+
+    setTimeout(function() {
+      window.location = el.href;
+    }, 200);
+
+  }
 
 
   /*
