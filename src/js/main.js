82934b0099c813bb49c735892648846787144696
Franklin Javier <franklinjalves@gmail.com>
2016-11-19 05:03:43 -0200

    fix: deploy process

--- src/js/main.js
+++ src/js/main.js
@@ -54,3 +54,30 @@
   });
 
 }());
+  
+/*
+ * Load Disqus dinamically when scroll reach bottom of the pages
+ */
+(function() {
+  'use strict';
+
+  if (!window.disqus_shortname && !window.disqus_url) return;
+
+  function preloadDisqus() {
+    var dsq = document.createElement('script'); 
+    dsq.type = 'text/javascript'; 
+    dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+
+    // remove event
+    window.onscroll = function() {};
+  }
+
+  window.onscroll = function(e) {
+    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500)) {
+      preloadDisqus();
+    }
+  };
+
+}());
